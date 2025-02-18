4f25143304a6408b123112e6a00c6f8f26938aae
tandrianatoandro <henimbola@gmail.com>
2025-02-18 13:44:16 +0400

    refactor: remove unused property

--- src/main/java/webhttp/WebHttpApplication.java
+++ src/main/java/webhttp/WebHttpApplication.java
@@ -1,22 +1,18 @@
 // src/main/java/webhttp/WebHttpApplication.java
 package webhttp;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.buffer.Unpooled;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.handler.codec.http.*;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
 
 public class WebHttpApplication {
 
     private static final int PORT = 8080;
-    private static final ObjectMapper objectMapper = new ObjectMapper();
 
     public static void run() throws Exception {
         EventLoopGroup bossGroup = new NioEventLoopGroup(1);
