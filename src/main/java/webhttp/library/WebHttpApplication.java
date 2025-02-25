a4c6273c0f9f34bc7b691a4b408db5b6bb9face3
tandrianatoandro <henimbola@gmail.com>
2025-02-25 23:03:30 +0400

    Refactor WebHttpApplication to use try-with-resources and handle specific exceptions

--- src/main/java/webhttp/library/WebHttpApplication.java
+++ src/main/java/webhttp/library/WebHttpApplication.java
@@ -10,11 +10,11 @@ import io.netty.handler.logging.LoggingHandler;
 
 public class WebHttpApplication {
 
-    public static void run(int port) throws Exception {
-        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
-        EventLoopGroup workerGroup = new NioEventLoopGroup();
-
-        try {
+    public static void run(int port) throws InterruptedException {
+        try (
+                EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+                EventLoopGroup workerGroup = new NioEventLoopGroup();
+        ) {
             ServerBootstrap b = new ServerBootstrap();
             b.option(ChannelOption.SO_BACKLOG, 1024);
             b.group(bossGroup, workerGroup)
@@ -25,9 +25,6 @@ public class WebHttpApplication {
             Channel ch = b.bind(port).sync().channel();
             System.out.println("Your server is available at http://localhost:" + port + '/');
             ch.closeFuture().sync();
-        } finally {
-            bossGroup.shutdownGracefully();
-            workerGroup.shutdownGracefully();
         }
     }
 }
